39b0b0d084527c4edb033e9285195fb23edd5d1c
catmanisacatlord <catmanisacatlord@gmail.com>
2020-10-28 17:29:20 -0600

    set up pogmessage to allow for more functionallity of the pog command

--- src/main/java/io/github/catmaniscatlord/pog/PogCommand.java
+++ src/main/java/io/github/catmaniscatlord/pog/PogCommand.java
@@ -1,5 +1,7 @@
 package io.github.catmaniscatlord.pog;
 
+import java.util.Arrays;
+
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.CommandExecutor;
@@ -10,32 +12,8 @@ import org.bukkit.ChatColor;
 
 public class PogCommand implements CommandExecutor{
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if(args.length < 1){
-            sender.sendMessage("pog");
-        }
-        else{
-            if(Bukkit.getPlayer(args[0]) != null){
-                Player target = Bukkit.getPlayer(args[0]);
-                String message = sender.getName() + " has Pogged you!";
-                if(args.length > 1)
-                {
-                    message += "\n" + pogMessage(args);
-                }
-                sender.sendMessage("You have pogged " + args[0]);
-                target.sendMessage(message);
-            }
-        }       
+        PogMessage pog = new PogMessage(sender , args[0], Arrays.copyOfRange(args, 1 ,args.length), true, true);   
+        pog.loop();
         return true;
-    }
-
-    private String pogMessage(String[] args){
-        String message = "";
-    
-        for(int i=1; i < args.length;i++)
-        {
-            message += args[i] + " ";
-        }
-        
-        return  ChatColor.translateAlternateColorCodes('&',message+"&r") + "pog!";
     }
 }
---
+++ src/main/java/io/github/catmaniscatlord/pog/PogMessage.java
@@ -0,0 +1,114 @@
+package io.github.catmaniscatlord.pog;
+
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.ChatColor;
+import org.bukkit.Bukkit;
+
+public class PogMessage{
+    private CommandSender sender;
+    private String target;
+    private String message = "";
+    private String userMessage;
+    private boolean broadcast;
+    private boolean targetIsPlayer;
+    private boolean userRun;
+
+    public PogMessage(CommandSender sender, String target, String[] userMessage, boolean broadcast, boolean userRun) {
+        this.sender = sender;
+        setTarget(target);
+        setUserMessage(userMessage);
+        this.broadcast = broadcast;
+        this.userRun = userRun; 
+    }
+
+    public void loop()
+    {
+        createMessage();
+        if (this.broadcast) {
+            Bukkit.broadcastMessage(this.message);
+        }
+        else {
+            if(userRun) {
+                this.sender.sendMessage(this.message);
+            }
+            if (this.targetIsPlayer) {
+                Player target = Bukkit.getPlayer(this.target);
+                target.sendMessage(this.message);
+            }
+        }
+    }
+
+    private void createMessage() {
+        if(userRun) {
+            if(!(this.sender instanceof Player)) {
+                this.message += ChatColor.AQUA + "The Server " + ChatColor.RESET;
+            }
+            else {
+                this.message += ChatColor.AQUA + this.sender.getName() + " " + ChatColor.RESET;
+            }
+            this.message += "has pogged "+ ChatColor.AQUA + this.target + ChatColor.RESET + "\n";
+        }
+        else {
+            this.message += ChatColor.GOLD + "Pog" + ChatColor.RESET;
+        }
+        this.message += ChatColor.BLUE + this.userMessage + ChatColor.RESET;
+    }
+    
+    /**
+     * check if the message has the formatting character then formats the string accordingly 
+     * 
+     */
+    private void colorMessage() {
+        if(this.userMessage.contains("&")) {
+            this.userMessage = ChatColor.translateAlternateColorCodes('&',this.userMessage+"&r");
+        }
+    }
+
+    /**
+     * checks if the target is a player, if it is then it checks the targetIsPlayer value to true 
+     * then it sets the target to the given string
+     * @param target the target
+     */
+    public void setTarget(String target) {
+        if(Bukkit.getPlayer(target) != null)
+        {
+            this.targetIsPlayer = true;
+        }
+        this.target = target;
+    }
+
+    /**
+     * sets the pog message based off of a string 
+     *
+     * @param message the message
+     */
+    public void setUserMessage(String userMessage) {
+        this.userMessage = userMessage;
+        colorMessage();
+    }
+
+    /**
+     * sets the pog message based off of a string array
+     *
+     * @param message the message in a string array
+     */
+    public void setUserMessage(String[] userMessage) {
+        if(userMessage == null)
+        for(String i : userMessage)
+        {
+            this.userMessage += i + " ";
+        }
+        this.userMessage.trim();
+        colorMessage();
+    }
+
+    /**
+     * sets wether or not the message is broadcast
+     *
+     * @param broadcast
+     */
+    public void setBroadcast(boolean broadcast) {
+        this.broadcast = broadcast;
+    }
+}
