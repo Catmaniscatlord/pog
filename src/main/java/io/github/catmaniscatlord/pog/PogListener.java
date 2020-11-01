916ca76ad3c4dc5e392c521fb7438571e7ac300e
catmanisacatlord <catmanisacatlord@gmail.com>
2020-11-01 01:27:47 -0600

    made it so the diamonds pog event can only happen every minute per player

--- src/main/java/io/github/catmaniscatlord/pog/PogListener.java
+++ src/main/java/io/github/catmaniscatlord/pog/PogListener.java
@@ -3,20 +3,39 @@ package io.github.catmaniscatlord.pog;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.entity.Player;
 import org.bukkit.Material; 
 import org.bukkit.ChatColor;
+import java.util.Map;
+import java.util.HashMap;
 
 public class PogListener implements Listener
 {
+    private HashMap<Player,Long> playerTimer = new HashMap<Player,Long>();
+    
     @EventHandler
     public void blockBreakEvent(BlockBreakEvent event)
     {
         String message;
         if(event.getBlock().getType().equals(Material.DIAMOND_ORE)) 
         {
-            message =  "§b§lDiamonds!!!";
-            PogMessage pog = new PogMessage(null, event.getPlayer().getName(), message, false, false);
-            pog.loop();
+            if(playerTimer.containsKey(event.getPlayer()))
+            {
+                if(System.currentTimeMillis() - playerTimer.get(event.getPlayer()) > 60*1000)
+                {
+                    message =  "§b§lDiamonds!!!";
+                    PogMessage pog = new PogMessage(null, event.getPlayer().getName(), message, false, false);
+                    pog.loop();
+                    playerTimer.put(event.getPlayer(),  System.currentTimeMillis());
+                } 
+            }
+            else
+            {
+                message =  "§b§lDiamonds!!!";
+                PogMessage pog = new PogMessage(null, event.getPlayer().getName(), message, false, false);
+                pog.loop();
+                playerTimer.put(event.getPlayer(), System.currentTimeMillis());
+            }
         }
     }
 }
